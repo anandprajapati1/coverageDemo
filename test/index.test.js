1ae497b91fa48f4a12dc5bc693d29c79ce9ff6ea
Anand Kumar Prajapati <anapraja@publicisgroupe.net>
2019-06-25 12:41:16 +0530

    Travis setup

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,12 @@
+import { app } from "../src";
+
 /* eslint-disable no-undef */
 
 test('test1', () => {
+  app();
   expect(1+2).toBe(3);
 });
+
+// test('app test', ()=>{
+//   expect(app()).toBeTruthy();
+// });
